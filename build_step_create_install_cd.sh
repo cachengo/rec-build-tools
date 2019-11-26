alexandruavadanii/build-tools

install cd: Use same kernel/initrd for UEFI boot
We currently replace the stock vmlinuz/initrd.img with the ones
built using DIB; however we only replace them for isolinux boot,
leaving UEFI systems booting the stock images from `images/pxeboot/`.

Switch to using the same binaries for both BIOS and UEFI systems.
This applies to both x86_64 and aarch64 EFI systems.

While at it, update EFI grub.cfg to resemble isolinux menu.

Signed-off-by: Alexandru Avadanii <Alexandru.Avadanii@enea.com>
Change-Id: I305de509a627b36712d29b2f8d51a8c1de09c58e

build_step_create_install_cd.sh
@@ -86,6 +86,12 @@ if [ "${iso_arch}" != 'aarch64' ]; then
     cp -f $scriptdir/akraino_splash.png isolinux/splash.png
 fi
 
+# Update grub.cfg for EFI booting, similar to isolinux
+sed -i '/menuentry/{N;N;N;q}' EFI/BOOT/grub.cfg
+sed -i -e 's|Install CentOS 7|Beginning the cloud installation process|' \
+       -e '/vmlinuz/ s/$/ console=tty0 console=ttyS1,115200 console=ttyAMA0,115200/' \
+    EFI/BOOT/grub.cfg
+
 popd
 
 pushd $tmp
@@ -113,6 +119,9 @@ if [ "${iso_arch}" != 'aarch64' ]; then
     cp -fp boot/vmlinuz-${KVER} $iso_build_dir/isolinux/vmlinuz
     cp -fp boot/initrd-provisioning.img $iso_build_dir/isolinux/initrd.img
 fi
+rm -f $iso_build_dir/images/pxeboot/vmlinuz $iso_build_dir/images/pxeboot/initrd.img
+cp -fp boot/vmlinuz-${KVER} $iso_build_dir/images/pxeboot/vmlinuz
+cp -fp boot/initrd-provisioning.img $iso_build_dir/images/pxeboot/initrd.img
 rm -rf boot/
 
 echo "Generating boot iso"

cachengo/rec-build-tools

build_step_create_install_cd.sh
@@ -77,7 +77,7 @@ if [ "${iso_arch}" != 'aarch64' ]; then
         cp $scriptdir/isolinux/isolinux.cfg isolinux/isolinux.cfg
     else
         sed -i "s/^timeout.*/timeout 100/" isolinux/isolinux.cfg
-        sed -i "s/^ -  Press.*/Beginning the cloud installation process/" isolinux/boot.msg
+        sed -i "s/^ -  Press.*/Begin the cloud installation process/" isolinux/boot.msg
         sed -i "s/^#menu hidden/menu hidden/" isolinux/isolinux.cfg
         sed -i "s/menu default//" isolinux/isolinux.cfg
         sed -i "/^label linux/amenu default" isolinux/isolinux.cfg
@@ -85,6 +85,12 @@ if [ "${iso_arch}" != 'aarch64' ]; then
     fi
     cp -f $scriptdir/akraino_splash.png isolinux/splash.png
 fi
+
+# Update grub.cfg for EFI booting, similar to isolinux
+sed -i '/menuentry/{N;N;N;q}' EFI/BOOT/grub.cfg
+sed -i -e 's|Install CentOS 7|Begin the cloud installation process|' \
+       -e '/vmlinuz/ s/$/ console=tty0 console=ttyS1,115200 console=ttyAMA0,115200/' \
+    EFI/BOOT/grub.cfg
 
 popd
 
@@ -113,6 +119,9 @@ if [ "${iso_arch}" != 'aarch64' ]; then
     cp -fp boot/vmlinuz-${KVER} $iso_build_dir/isolinux/vmlinuz
     cp -fp boot/initrd-provisioning.img $iso_build_dir/isolinux/initrd.img
 fi
+rm -f $iso_build_dir/images/pxeboot/vmlinuz $iso_build_dir/images/pxeboot/initrd.img
+cp -fp boot/vmlinuz-${KVER} $iso_build_dir/images/pxeboot/vmlinuz
+cp -fp boot/initrd-provisioning.img $iso_build_dir/images/pxeboot/initrd.img
 rm -rf boot/
 
 echo "Generating boot iso"